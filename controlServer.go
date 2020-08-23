b7959629af49eb58d995a118959d29b3caa267b4
Jeremy Williamson <jeremy.williamson83@gmail.com>
2020-08-23 02:29:13 -0400

    added controlserver

---
+++ controlServer.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+//StartServer starts the control server
+func StartServer() {
+	PORT := ":" + "1337"
+	listener, err := net.Listen("tcp", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer listener.Close()
+
+	connection, err := listener.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		netData, err := bufio.NewReader(connection).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		command := strings.TrimSpace(string(netData))
+
+		fmt.Print("-> ", command, "\n")
+
+		var response string
+		switch command {
+		case "exit":
+			{
+				fmt.Println("Exiting TCP server!")
+				response = fmt.Sprintf("exit")
+				connection.Write([]byte(response))
+				connection.Close()
+				return
+			}
+		default:
+			{
+				response = fmt.Sprintln("unknown command:", command)
+				connection.Write([]byte(response))
+			}
+		}
+	}
+}
