7d52bcd6b0185b2e84c101e36f16e9bb93454156
Jeremy Williamson <jeremy.williamson83@gmail.com>
2020-08-28 21:30:21 -0400

    removed lists of specific objects and only using one list of gameObject interfaces

--- gameObject.go
+++ gameObject.go
@@ -17,7 +17,7 @@ const (
 )
 
 const (
-	maxGameObjects = 800
+	maxGameObjects = 400
 	maxInitiative  = 10.0
 	maxSpeed       = 100.0
 	maxStamina     = 100.0
@@ -31,7 +31,7 @@ type gameObject interface {
 	getID() int
 	setHitBox()
 	getHitBox() pixel.Rect
-	update(dt float64, gameObjects *GameObjects, waitGroup *sync.WaitGroup)
+	update(dt float64, gameObjects GameObjects, waitGroup *sync.WaitGroup)
 	changeState(newState objectState)
 	draw(win *pixelgl.Window, drawHitBox bool, waitGroup *sync.WaitGroup)
 }
@@ -47,10 +47,34 @@ func (gameObjs GameObjects) fastRemoveIndex(index int) GameObjects {
 
 func (gameObjs GameObjects) appendGameObject(newObject gameObject) GameObjects {
 	if len(gameObjs) >= maxGameObjects {
-		return nil
+		return gameObjs
 	}
 	gameObjs = append(gameObjs, newObject)
 	return gameObjs
+}
+
+func (gameObjs GameObjects) appendLivingObject(animationKeys []string, animations map[string][]pixel.Rect, sheet pixel.Picture, position pixel.Vec) GameObjects {
+	newLivingObject := creatNewLivingObject(animationKeys, animations, sheet, position)
+	return gameObjs.appendGameObject(&newLivingObject)
+}
+
+func (gameObjs GameObjects) appendGibletObject(animationKeys []string, animations map[string][]pixel.Rect, sheet pixel.Picture, position pixel.Vec) GameObjects {
+	newGibletObject := creatNewGibletObject(animationKeys, animations, sheet, position)
+	return gameObjs.appendGameObject(&newGibletObject)
+}
+
+func (gameObjs GameObjects) updateAllObjects(dt float64, waitGroup *sync.WaitGroup) {
+	for _, currentObj := range gameObjs {
+		waitGroup.Add(1)
+		go currentObj.update(dt, gameObjs, waitGroup)
+	}
+}
+
+func (gameObjs GameObjects) drawAllObjects(win *pixelgl.Window, drawHitBox bool, waitGroup *sync.WaitGroup) {
+	for _, obj := range gameObjs {
+		waitGroup.Add(1)
+		go obj.draw(win, drawHitBox, waitGroup)
+	}
 }
 
 func (gameObjs GameObjects) getSelectedGameObj(position pixel.Vec) (gameObject, int, bool, error) {
