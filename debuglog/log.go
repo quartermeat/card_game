19d086aec0edec865403a832f2500eafa5480eab
Jeremy Williamson <jeremy.williamson83@gmail.com>
2023-04-24 20:48:00 -0400

    went back to what was working

--- debuglog/log.go
+++ debuglog/log.go
@@ -1,14 +1,22 @@
+// Package 'errormgmt' Error management, probably the beginnings of what is really a debug log
+// TODO: refactor to debug log
 package debuglog
 
-import "sync"
+// IEntry interface for the custom error, may change to 'log entry'
+// may allow a return of 'status' or 'error' based on type
+type IEntry interface {
+	GetMessage() string
+}
 
-var instance *Entries
-var once sync.Once
+// Entries is instantiated in main to be global list of errors/debug log statments made
+type Entries []IEntry
 
-// GetDebugLog returns the singleton instance of the debug log
-func GetDebugLog() *Entries {
-	once.Do(func() {
-		instance = &Entries{}
-	})
-	return instance
+// Entry is the entry structure
+type Entry struct {
+	Message string
+}
+
+// GetMessage returns the message in the entry struct
+func (entry Entry) GetMessage() string {
+	return entry.Message
 }
